1e5694ea1f28cf03b778a0d5382adb9ba1523cfc
Peter Downey <peterdowney@visistruct.com>
2021-10-06 04:09:32 -0400

    Condensing wp-config.php

--- var/www/wordpress/wp-config.php
+++ var/www/wordpress/wp-config.php
@@ -4,15 +4,15 @@
 // https://EngineScript.com
 
 /* MySQL settings - You can get this info from your web host */
-define('DB_NAME',			'SEDWPDB');
-define('DB_USER',			'SEDWPUSER');
+define('DB_NAME',	'SEDWPDB');
+define('DB_USER',	'SEDWPUSER');
 define('DB_PASSWORD',	'SEDWPPASS');
 define('DB_CHARSET',	'utf8mb4');
-define('DB_HOST',			'localhost');
+define('DB_HOST',	'localhost');
 define('DB_COLLATE',	'');
 
 /* MySQL database table prefix. */
-$table_prefix  = 'SEDPREFIX_';
+$table_prefix = 'SEDPREFIX_';
 
 /* Site Details */
 define('WP_HOME', 'https://SEDURL');
@@ -42,12 +42,12 @@ define('FORCE_SSL_LOGIN', true);
 define('WP_ALLOW_MULTISITE', false);
 
 /* Redis Object Cache	*/
-define('WP_CACHE_KEY_SALT', 'SEDURL');
-define('WP_REDIS_DISABLE_BANNERS', 'true');
-define('WP_REDIS_IGBINARY', 'true');
-define('WP_REDIS_MAXTTL', '300');
-//define('WP_REDIS_PATH', '/run/redis/redis-server.sock');
-define('WP_REDIS_PREFIX', 'SEDURL');
+define('WP_CACHE_KEY_SALT',	'SEDURL');
+define('WP_REDIS_DISABLE_BANNERS',	'true');
+define('WP_REDIS_IGBINARY',	'true');
+define('WP_REDIS_MAXTTL',	'300');
+//define('WP_REDIS_PATH',	'/run/redis/redis-server.sock');
+define('WP_REDIS_PREFIX',	'SEDURL');
 //define('WP_REDIS_SCHEME', 'unix');
 define('WP_REDIS_SELECTIVE_FLUSH', 'true');
 
@@ -76,12 +76,12 @@ define('FS_CHMOD_DIR', 0755);
 define('FS_CHMOD_FILE', 0644);
 
 /* Content */
-define('AUTOSAVE_INTERVAL', 60);			// Time in seconds
-define('EMPTY_TRASH_DAYS', 14);				// Setting to 0 causes all deletions skip the trash folder and are permanent.
+define('AUTOSAVE_INTERVAL', 60);	// Time in seconds
+define('EMPTY_TRASH_DAYS', 14);	// Setting to 0 causes all deletions skip the trash folder and are permanent.
 define('IMAGE_EDIT_OVERWRITE', true);
 define('MEDIA_TRASH', true);
-define('WP_POST_REVISIONS', 2);				// Can also be set to false
-define('ALLOW_UNFILTERED_UPLOADS', false);  // Allows admins to upload files that would normally be filtered by WordPress by default.
+define('WP_POST_REVISIONS', 2);	// Can also be set to false
+define('ALLOW_UNFILTERED_UPLOADS', false);	// Allows admins to upload files that would normally be filtered by WordPress by default.
 
 /* Disable Nag Notices */
 // https://codex.wordpress.org/Plugin_API/Action_Reference/admin_notices
@@ -94,19 +94,19 @@ define('WP_CLI_BIN_DIR', '/tmp/wp-cli-phar');
 define('WP_CLI_CONFIG_PATH', '/tmp/wp-cli-phar/config.yml');
 
 /* Debug */
-define('WP_DEBUG', false);						// Set to true if you want to debug
-define('CONCATENATE_SCRIPTS', true);  // Setting to false may fix java issues in dashboard only
-define('SAVEQUERIES', false);         // https://codex.wordpress.org/Editing_wp-config.php#Save_queries_for_analysis
-define('SCRIPT_DEBUG', false);        // Use dev versions of core JS and CSS files (only needed if you are modifying these core files)
-define('WP_ALLOW_REPAIR', false);     // https://SEDURL/wp-admin/maint/repair.php  - Make sure to disable this once you're done. Anyone can trigger this.
-define('WP_DEBUG_DISPLAY', false);    // Displays logs within browser on site. Not for production environments.
+define('WP_DEBUG', false);	// Set to true if you want to debug
+define('CONCATENATE_SCRIPTS', true);	// Setting to false may fix java issues in dashboard only
+define('SAVEQUERIES', false);	// https://codex.wordpress.org/Editing_wp-config.php#Save_queries_for_analysis
+define('SCRIPT_DEBUG', false);	// Use dev versions of core JS and CSS files (only needed if you are modifying these core files)
+define('WP_ALLOW_REPAIR', false);	// https://SEDURL/wp-admin/maint/repair.php - Make sure to disable this once you're done. Anyone can trigger this.
+define('WP_DEBUG_DISPLAY', false);	// Displays logs within browser on site. Not for production environments.
 define('WP_DEBUG_LOG', '/var/log/domains/SEDURL/SEDURL-wp-error.log' ); // Only writes log if WP_DEBUG is set to true.
 
 /* Compression */
 // Leave these disabled unless you absolutely need them for whatever reason. This is done with Nginx and Cloudflare.
-//define('COMPRESS_CSS',			true);
+//define('COMPRESS_CSS',	true);
 //define('COMPRESS_SCRIPTS',	true);
-//define('ENFORCE_GZIP',			true);
+//define('ENFORCE_GZIP',	true);
 
 /* Security Headers */
 // Leave these disabled unless you absolutely need them for whatever reason. This is done with Nginx and Cloudflare.
