65de2cd8f2055b486f61ac2b1c6c4f78d337d9ec
Peter Downey <peterdowney@visistruct.com>
2022-05-13 19:14:11 -0400

    Cleaned up Redis config

--- scripts/install/redis/redis-install.sh
+++ scripts/install/redis/redis-install.sh
@@ -29,16 +29,19 @@ apt install -qy redis-server redis-tools --no-install-recommends
 
 # Setup Redis
 mkdir -p /run/redis
+mkdir -p /var/lib/redis
 mkdir -p /var/log/redis
 touch /var/log/redis/redis.log
-find /var/log/redis -type d,f -exec chmod 755 {} \;
-chmod 775 /etc/redis/redis.conf
+find /var/log/redis -type d,f -exec chmod 775 {} \;
 chmod 775 /run/redis
+chmod 775 /var/lib/redis
 chown -R redis:redis /run/redis
+chown -R redis:redis /var/lib/redis
 chown -R redis:redis /var/log/redis
 
 cp -rf /usr/local/bin/enginescript/etc/redis/redis.conf /etc/redis/redis.conf
 sed -i "s|SEDREDISMAXMEM|${SERVER_MEMORY_TOTAL_06}|g" /etc/redis/redis.conf
 chown -R redis:redis /etc/redis/redis.conf
+chmod 775 /etc/redis/redis.conf
 service redis-server restart
 sudo systemctl enable redis-server
