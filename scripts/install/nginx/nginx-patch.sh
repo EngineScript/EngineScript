4edc685aaacc4b4eec41832eae227376e791237d
Peter Downey <peterdowney@visistruct.com>
2022-07-03 22:26:54 -0400

    Fixed Headers More Patch

--- scripts/install/nginx/nginx-patch.sh
+++ scripts/install/nginx/nginx-patch.sh
@@ -30,6 +30,8 @@ fi
 cd /usr/src/nginx-${NGINX_VER}
 patch -p1 < /usr/local/bin/enginescript/patches/nginx.patch
 patch -p1 < /usr/local/bin/enginescript/patches/nginx_io_uring.patch
+
+cd /usr/src/headers-more-nginx-module-0.33
 patch -p1 < /usr/local/bin/enginescript/patches/nginx_headers_more.patch
 
 # Patch OpenSSL
