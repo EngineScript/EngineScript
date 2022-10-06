5aa9d3ed27694ed61310a68260825f019a830b04
Peter Downey <peterdowney@visistruct.com>
2022-10-06 00:14:57 -0400

    Added local backup option

--- scripts/install/cron/cron-install.sh
+++ scripts/install/cron/cron-install.sh
@@ -33,6 +33,30 @@ cp -rf /usr/local/bin/enginescript/scripts/cron/sites.sh /home/EngineScript/site
 # WordPress Cron Ping (every 36 minutes)
 (crontab -l 2>/dev/null; echo "*/36 * * * * cd /usr/local/bin/enginescript/scripts/cron; bash wp-cron.sh >/dev/null 2>&1") | crontab -
 
+# Local Backups
+if [ "${AUTOMATIC_LOCAL_BACKUPS}" = 1 ];
+  then
+    (crontab -l 2>/dev/null; echo "30 2 */5 * * cd /usr/local/bin/enginescript/scripts/cron; bash optimize-images.sh >/dev/null 2>&1") | crontab -
+  else
+    # Do nothing!
+    echo "Skipping Automatic Image Optimization cron install"
+fi
+
+# Dropbox Backups (daily)
+if [ "${INSTALL_DROPBOX_BACKUP}" = 1 ];
+  then
+    (crontab -l 2>/dev/null; echo "0 4 * * * cd /usr/local/bin/enginescript/scripts/cron; bash backups.sh >/dev/null 2>&1") | crontab -
+  else
+    # Do nothing!
+    echo "Skipping Dropbox Uploader cron install"
+fi
+
+# Backup Nginx Configuration (daily)
+(crontab -l 2>/dev/null; echo "0 6 * * * cd /usr/local/bin/enginescript/scripts/cron; bash nginx-backup.sh >/dev/null 2>&1") | crontab -
+
+# Backup PHP Configuration (daily)
+(crontab -l 2>/dev/null; echo "15 6 * * * cd /usr/local/bin/enginescript/scripts/cron; bash php-backup.sh >/dev/null 2>&1") | crontab -
+
 # Lossless Image Optimization on Web Directories
 # (Only runs on new images since last run) (every 5 days)
 if [ "${AUTOMATIC_LOSSLESS_IMAGE_OPTIMIZATION}" = 1 ];
@@ -52,18 +76,6 @@ fi
 # Retrieve  Cloudflare Server IP Ranges for UFW (monthly)
 (crontab -l 2>/dev/null; echo "2 3 1 * * cd /usr/local/bin/enginescript/scripts/cron; bash ufw-cloudflare-cron.sh >/dev/null 2>&1") | crontab -
 
-# Backup WordPress Databases & Upload Directories (daily)
-(crontab -l 2>/dev/null; echo "0 4 * * * cd /usr/local/bin/enginescript/scripts/cron; bash backups.sh >/dev/null 2>&1") | crontab -
-
-# Backup WordPress Databases to Dropbox (daily)
-if [ "${INSTALL_DROPBOX_BACKUP}" = 1 ];
-  then
-    (crontab -l 2>/dev/null; echo "15 4 * * * cd /usr/local/bin/enginescript/scripts/cron; bash dropbox-backups.sh >/dev/null 2>&1") | crontab -
-  else
-    # Do nothing!
-    echo "Skipping Dropbox Uploader cron install"
-fi
-
 # Update WP-CLI & Packages (daily)
 (crontab -l 2>/dev/null; echo "45 4 * * * cd /usr/local/bin/enginescript/scripts/cron; bash wp-cli-update.sh >/dev/null 2>&1") | crontab -
 
@@ -72,9 +84,3 @@ fi
 
 # Check WordPress Directories Against Checksums (daily)
 (crontab -l 2>/dev/null; echo "15 5 * * * cd /usr/local/bin/enginescript/scripts/cron; bash checksums.sh >/dev/null 2>&1") | crontab -
-
-# Backup Nginx Configuration (daily)
-(crontab -l 2>/dev/null; echo "0 6 * * * cd /usr/local/bin/enginescript/scripts/cron; bash nginx-backup.sh >/dev/null 2>&1") | crontab -
-
-# Backup PHP Configuration (daily)
-(crontab -l 2>/dev/null; echo "15 6 * * * cd /usr/local/bin/enginescript/scripts/cron; bash php-backup.sh >/dev/null 2>&1") | crontab -
