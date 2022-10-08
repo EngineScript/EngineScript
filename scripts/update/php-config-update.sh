3e6e93d9b298beb37f2a482f67a957d112192b61
Peter Downey <peterdowney@visistruct.com>
2022-10-08 19:26:41 -0400

    PHP Tuning Added an attempt at calculating decent php settings.

--- scripts/update/php-config-update.sh
+++ scripts/update/php-config-update.sh
@@ -24,10 +24,30 @@ fi
 #----------------------------------------------------------------------------
 # Start Main Script
 
+# Calculate PHP FPM tune depending on RAM
+calculate_php() {
+available_memory=$(awk '/MemAvailable/ {printf "%d", $2/1024}' /proc/meminfo)
+average_php_memory_requirement=50
+CPU_COUNT="$(nproc --all)"
+PHP_FPM_MAX_CHILDREN_ALT=$((available_memory/average_php_memory_requirement))
+PHP_FPM_MAX_CHILDREN=$(( "$(free -m | awk 'NR==2{printf "%d", $2/65 }')" ))
+PHP_FPM_SPARE_SERVERS=$(( "$(nproc --all)" * 2 ))
+PHP_FPM_START_SERVERS=$(( "$(nproc --all)" * 4 ))
+SERVER_MEMORY_TOTAL_017=$(( "$(free -m | awk 'NR==2{printf "%d", $2*0.017 }')" ))
+SERVER_MEMORY_TOTAL_03=$(( "$(free -m | awk 'NR==2{printf "%d", $2*0.03 }')" ))
+SERVER_MEMORY_TOTAL_13=$(( "$(free -m | awk 'NR==2{printf "%d", $2*0.13/.50 }')" ))
+
+sed -i "s|pm.max_children = 10|pm.max_children = ${PHP_FPM_MAX_CHILDREN}|g" /etc/php/${PHP_VER}/fpm/pool.d/www.conf
+sed -i "s|pm.start_servers = 4|pm.start_servers = ${PHP_FPM_START_SERVERS}|g" /etc/php/${PHP_VER}/fpm/pool.d/www.conf
+sed -i "s|pm.min_spare_servers = 2|pm.min_spare_servers = ${PHP_FPM_SPARE_SERVERS}|g" /etc/php/${PHP_VER}/fpm/pool.d/www.conf
+sed -i "s|pm.max_spare_servers = 4|pm.max_spare_servers = ${PHP_FPM_START_SERVERS}|g" /etc/php/${PHP_VER}/fpm/pool.d/www.conf
+sed -i "s|SEDOPCACHEINTBUF|${SERVER_MEMORY_TOTAL_017}|g" /etc/php/${PHP_VER}/fpm/php.ini
+sed -i "s|SEDOPCACHEJITBUFFER|${SERVER_MEMORY_TOTAL_03}|g" /etc/php/${PHP_VER}/fpm/php.ini
+sed -i "s|SEDOPCACHEMEM|${SERVER_MEMORY_TOTAL_13}|g" /etc/php/${PHP_VER}/fpm/php.ini
+}
+
 # Update PHP config
 cp -rf /usr/local/bin/enginescript/etc/php/php.ini /etc/php/${PHP_VER}/fpm/php.ini
 cp -rf /usr/local/bin/enginescript/etc/php/php-fpm.conf /etc/php/${PHP_VER}/fpm/php-fpm.conf
 cp -rf /usr/local/bin/enginescript/etc/php/www.conf /etc/php/${PHP_VER}/fpm/pool.d/www.conf
-sed -i "s|SEDOPCACHEJITBUFFER|${SERVER_MEMORY_TOTAL_03}|g" /etc/php/${PHP_VER}/fpm/php.ini
-sed -i "s|SEDOPCACHEMEM|${SERVER_MEMORY_TOTAL_13}|g" /etc/php/${PHP_VER}/fpm/php.ini
-sed -i "s|SEDOPCACHEINTBUF|${SERVER_MEMORY_TOTAL_017}|g" /etc/php/${PHP_VER}/fpm/php.ini
+calculate_php
