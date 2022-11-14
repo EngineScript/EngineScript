d99d84ed0bd3dee760bbb14df3dbeadf2afd5aa9
Peter Downey <peterdowney@visistruct.com>
2022-11-14 02:36:06 -0500

    opcache tuning

--- scripts/update/php-config-update.sh
+++ scripts/update/php-config-update.sh
@@ -33,7 +33,7 @@ PHP_FPM_MAX_CHILDREN_ALT=$((AVAILABLE_MEMORY/AVERAGE_PHP_MEMORY_REQ))
 PHP_FPM_MAX_CHILDREN=$(( "$(free -m | awk 'NR==2{printf "%d", $2/65 }')" ))
 PHP_FPM_SPARE_SERVERS=$(( "$(nproc --all)" * 2 ))
 PHP_FPM_START_SERVERS=$(( "$(nproc --all)" * 4 ))
-SERVER_MEMORY_TOTAL_017=$(( "$(free -m | awk 'NR==2{printf "%d", $2*0.017 }')" ))
+SERVER_MEMORY_TOTAL_01="$(free -m | awk 'NR==2{printf "%d", $2*0.01 }')"
 SERVER_MEMORY_TOTAL_03=$(( "$(free -m | awk 'NR==2{printf "%d", $2*0.03 }')" ))
 SERVER_MEMORY_TOTAL_13=$(( "$(free -m | awk 'NR==2{printf "%d", $2*0.13 }')" ))
 
@@ -41,9 +41,16 @@ sed -i "s|pm.max_children = 10|pm.max_children = ${PHP_FPM_MAX_CHILDREN}|g" /etc
 sed -i "s|pm.start_servers = 4|pm.start_servers = ${PHP_FPM_START_SERVERS}|g" /etc/php/${PHP_VER}/fpm/pool.d/www.conf
 sed -i "s|pm.min_spare_servers = 2|pm.min_spare_servers = ${PHP_FPM_SPARE_SERVERS}|g" /etc/php/${PHP_VER}/fpm/pool.d/www.conf
 sed -i "s|pm.max_spare_servers = 4|pm.max_spare_servers = ${PHP_FPM_START_SERVERS}|g" /etc/php/${PHP_VER}/fpm/pool.d/www.conf
-sed -i "s|SEDOPCACHEINTBUF|${SERVER_MEMORY_TOTAL_017}|g" /etc/php/${PHP_VER}/fpm/php.ini
 sed -i "s|SEDOPCACHEJITBUFFER|${SERVER_MEMORY_TOTAL_03}|g" /etc/php/${PHP_VER}/fpm/php.ini
 sed -i "s|SEDOPCACHEMEM|${SERVER_MEMORY_TOTAL_13}|g" /etc/php/${PHP_VER}/fpm/php.ini
+
+if [ "${SERVER_MEMORY_TOTAL_80}" -lt 4000 ];
+  then
+    sed -i "s|SEDOPCACHEINTBUF|16|g" /etc/php/${PHP_VER}/fpm/php.ini
+  else
+    sed -i "s|SEDOPCACHEINTBUF|${SERVER_MEMORY_TOTAL_01}|g" /etc/php/${PHP_VER}/fpm/php.ini
+fi
+
 }
 
 # Update PHP config
