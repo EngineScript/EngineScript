6113f910ebfaa226262a58359039fa56d82aff1c
Peter Downey <peterdowney@visistruct.com>
2023-03-15 18:05:10 -0400

    Updated Cloudflare IP UFW script

--- scripts/install/ufw/ufw-cloudflare.sh
+++ scripts/install/ufw/ufw-cloudflare.sh
@@ -25,27 +25,9 @@ fi
 # Start Main Script
 
 # Add UFW Rules for Cloudflare
-# Credit: https://github.com/simnix/cloudflare-ufw/
-
-# Retrieve IPs
-curl -s https://www.cloudflare.com/ips-v4 -o /tmp/cf_ips
-echo "" >> /tmp/cf_ips
-curl -s https://www.cloudflare.com/ips-v6 >> /tmp/cf_ips
+# Credit: https://github.com/Paul-Reed/cloudflare-ufw
 
 # Allow all traffic from Cloudflare IPs (no ports restriction)
-for cfip in `cat /tmp/cf_ips`; do ufw allow proto tcp from $cfip comment 'Cloudflare IP'; done
+for cfip in `curl -sw '\n' https://www.cloudflare.com/ips-v{4,6}`; do ufw allow proto tcp from $cfip comment 'Cloudflare IP'; done
 
 ufw reload > /dev/null
-
-# OTHER EXAMPLE RULES
-# Retrict to port 80 (HTTP)
-#for cfip in `cat /tmp/cf_ips`; do ufw allow proto tcp from $cfip to any port 80/tcp comment 'Cloudflare IP'; done
-
-# Restrict to port 443 (HTTPS)
-#for cfip in `cat /tmp/cf_ips`; do ufw allow from proto tcp $cfip to any port 443/tcp comment 'Cloudflare IP'; done
-
-# Restrict to port 2048 (Railgun)
-#for cfip in `cat /tmp/cf_ips`; do ufw allow proto tcp from $cfip to any port 2048/tcp comment 'Cloudflare IP'; done
-
-# Restrict to ports 80, 443, 2048
-#for cfip in `cat /tmp/cf_ips`; do ufw allow proto tcp from $cfip to any port 80,443,2048/tcp comment 'Cloudflare IP'; done
