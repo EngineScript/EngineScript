0f47132d9f3804cb98965e7ab53a8e89f07cdb76
Peter Downey <peterdowney@visistruct.com>
2023-05-08 18:32:33 -0400

    Journal size

---
+++ scripts/install/systemd/journald.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+#----------------------------------------------------------------------------
+# EngineScript - High-Performance WordPress LEMP Server
+#----------------------------------------------------------------------------
+# Website:      https://EngineScript.com
+# GitHub:       https://github.com/Enginescript/EngineScript
+# Company:      VisiStruct / EngineScript
+# License:      GPL v3.0
+# OS:           Ubuntu 22.04 (jammy)
+#----------------------------------------------------------------------------
+
+# EngineScript Variables
+source /usr/local/bin/enginescript/enginescript-variables.txt
+source /home/EngineScript/enginescript-install-options.txt
+
+# Check current user's ID. If user is not 0 (root), exit.
+if [ "${EUID}" != 0 ];
+  then
+    echo "${BOLD}ALERT:${NORMAL}"
+    echo "EngineScript should be executed as the root user."
+    exit
+fi
+
+#----------------------------------------------------------------------------
+# Start Main Script
+
+# Restrict Journal Log Size
+sed -i "s|#SystemMaxUse=|SystemMaxUse=500M|g" /etc/systemd/journald.conf
+journalctl --vacuum-time=2days
+systemctl daemon-reload
