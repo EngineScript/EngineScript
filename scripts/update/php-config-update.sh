ac60590ddd91cb259a48535fee8bbf507ff669df
Peter Downey <peterdowney@visistruct.com>
2023-10-10 00:24:20 -0400

    PHP Tuning
    Overhauled the PHP settings. Switched from dynamic to ondemand. 
    
    Also raised upload limits to 512mb.

--- scripts/update/php-config-update.sh
+++ scripts/update/php-config-update.sh
@@ -29,27 +29,68 @@ calculate_php() {
   AVAILABLE_MEMORY=$(awk '/MemAvailable/ {printf "%d", $2/1024}' /proc/meminfo)
   AVERAGE_PHP_MEMORY_REQ=80
   CPU_COUNT="$(nproc --all)"
-  PHP_FPM_MAX_CHILDREN_ALT=$((AVAILABLE_MEMORY/AVERAGE_PHP_MEMORY_REQ))
-  PHP_FPM_MAX_CHILDREN=$(( "$(free -m | awk 'NR==2{printf "%d", $2/80 }')" ))
+  #PHP_FPM_MAX_CHILDREN_ALT=$((AVAILABLE_MEMORY/AVERAGE_PHP_MEMORY_REQ))
+  #PHP_FPM_MAX_CHILDREN=$(( "$(free -m | awk 'NR==2{printf "%d", $2/80 }')" ))
   PHP_FPM_SPARE_SERVERS=$(( "$(nproc --all)" * 2 ))
   PHP_FPM_START_SERVERS=$(( "$(nproc --all)" * 4 ))
   SERVER_MEMORY_TOTAL_01="$(free -m | awk 'NR==2{printf "%d", $2*0.01 }')"
   SERVER_MEMORY_TOTAL_03=$(( "$(free -m | awk 'NR==2{printf "%d", $2*0.03 }')" ))
   SERVER_MEMORY_TOTAL_13=$(( "$(free -m | awk 'NR==2{printf "%d", $2*0.13 }')" ))
 
-  sed -i "s|pm.max_children = 10|pm.max_children = ${PHP_FPM_MAX_CHILDREN}|g" /etc/php/${PHP_VER}/fpm/pool.d/www.conf
+  #sed -i "s|pm.max_children = 10|pm.max_children = ${PHP_FPM_MAX_CHILDREN}|g" /etc/php/${PHP_VER}/fpm/pool.d/www.conf
   sed -i "s|pm.start_servers = 4|pm.start_servers = ${PHP_FPM_START_SERVERS}|g" /etc/php/${PHP_VER}/fpm/pool.d/www.conf
   sed -i "s|pm.min_spare_servers = 2|pm.min_spare_servers = ${PHP_FPM_SPARE_SERVERS}|g" /etc/php/${PHP_VER}/fpm/pool.d/www.conf
   sed -i "s|pm.max_spare_servers = 4|pm.max_spare_servers = ${PHP_FPM_START_SERVERS}|g" /etc/php/${PHP_VER}/fpm/pool.d/www.conf
-  sed -i "s|SEDOPCACHEJITBUFFER|${SERVER_MEMORY_TOTAL_03}|g" /etc/php/${PHP_VER}/fpm/php.ini
-  sed -i "s|SEDOPCACHEMEM|${SERVER_MEMORY_TOTAL_13}|g" /etc/php/${PHP_VER}/fpm/php.ini
 
+  # Tuning pm.max_children
+  # For Servers with 1GB RAM
+  if [ "${SERVER_MEMORY_TOTAL_100}" -lt 1000 ];
+    then
+      sed -i "s|pm.max_children = 10|pm.max_children = 6|g" /etc/php/${PHP_VER}/fpm/pool.d/www.conf
+  fi
+
+  # For Servers with 2GB RAM
+  if [ "${SERVER_MEMORY_TOTAL_100}" -lt 2000 ];
+    then
+      sed -i "s|pm.max_children = 10|pm.max_children = 12|g" /etc/php/${PHP_VER}/fpm/pool.d/www.conf
+  fi
+
+  # For Servers with 4GB RAM
+  if [ "${SERVER_MEMORY_TOTAL_100}" -lt 4000 ];
+    then
+      sed -i "s|pm.max_children = 10|pm.max_children = 22|g" /etc/php/${PHP_VER}/fpm/pool.d/www.conf
+  fi
+
+  # For Servers with 8GB RAM
+  if [ "${SERVER_MEMORY_TOTAL_100}" -lt 8000 ];
+    then
+      sed -i "s|pm.max_children = 10|pm.max_children = 44|g" /etc/php/${PHP_VER}/fpm/pool.d/www.conf
+  fi
+
+  # Memory Limit
+  # For Servers with 2GB RAM or less
+  if [ "${SERVER_MEMORY_TOTAL_100}" -lt 2000 ];
+    then
+      sed -i "s|memory_limit = SEDMEMLM|memory_limit = 265M|g" /etc/php/${PHP_VER}/fpm/pool.d/www.conf
+  fi
+
+  # For Servers with 8GB RAM
+  if [ "${SERVER_MEMORY_TOTAL_100}" -lt 8000 ];
+    then
+      sed -i "s|memory_limit = SEDMEMLM|memory_limit = 512M|g" /etc/php/${PHP_VER}/fpm/pool.d/www.conf
+  fi
+
+# OpCache Tuning
   if [ "${SERVER_MEMORY_TOTAL_80}" -lt 4000 ];
     then
       sed -i "s|SEDOPCACHEINTBUF|16|g" /etc/php/${PHP_VER}/fpm/php.ini
     else
       sed -i "s|SEDOPCACHEINTBUF|${SERVER_MEMORY_TOTAL_01}|g" /etc/php/${PHP_VER}/fpm/php.ini
   fi
+
+  sed -i "s|SEDOPCACHEJITBUFFER|${SERVER_MEMORY_TOTAL_03}|g" /etc/php/${PHP_VER}/fpm/php.ini
+  sed -i "s|SEDOPCACHEMEM|${SERVER_MEMORY_TOTAL_13}|g" /etc/php/${PHP_VER}/fpm/php.ini
+
 }
 
 # Update PHP config
