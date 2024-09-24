13cc756084de3b6f0469c2796aa682a9a7544a80
Peter Downey <peterdowney@visistruct.com>
2024-09-24 04:15:26 -0400

    GCC changes

--- scripts/install/gcc/gcc-install.sh
+++ scripts/install/gcc/gcc-install.sh
@@ -24,9 +24,28 @@ fi
 # Start Main Script
 
 # GCC
+
 # Remove previous GCC alternatives
 update-alternatives --remove-all gcc
 
-# Create new GCC alternatives
-update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-12 100 --slave /usr/bin/g++ g++ /usr/bin/g++-12 --slave /usr/bin/gcov gcov /usr/bin/gcov-12
-update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-11 90 --slave /usr/bin/g++ g++ /usr/bin/g++-11 --slave /usr/bin/gcov gcov /usr/bin/gcov-11
+UBUNTU_VERSION="$(lsb_release -sr)"
+if [ "${UBUNTU_VERSION}" = 22.04 ];
+  then
+    # Install GCC for Ubuntu 22.04
+    apt install g++-11 gcc-11 g++-12 gcc-12 -y
+
+    # Create new GCC alternatives
+    update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-12 100 --slave /usr/bin/g++ g++ /usr/bin/g++-12 --slave /usr/bin/gcov gcov /usr/bin/gcov-12
+    update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-11 90 --slave /usr/bin/g++ g++ /usr/bin/g++-11 --slave /usr/bin/gcov gcov /usr/bin/gcov-11
+fi
+
+UBUNTU_VERSION="$(lsb_release -sr)"
+if [ "${UBUNTU_VERSION}" = 24.04 ];
+  then
+    # Install GCC for Ubuntu 22.04
+    apt install g++-13 gcc-13 g++-14 gcc-14
+
+    # Create new GCC alternatives
+    update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-14 100 --slave /usr/bin/g++ g++ /usr/bin/g++-14 --slave /usr/bin/gcov gcov /usr/bin/gcov-14
+    update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-13 90 --slave /usr/bin/g++ g++ /usr/bin/g++-13 --slave /usr/bin/gcov gcov /usr/bin/gcov-13
+fi
