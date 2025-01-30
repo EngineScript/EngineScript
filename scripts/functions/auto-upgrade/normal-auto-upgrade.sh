24dff6207c93a6da2e9dedeb2aff110a1650b059
Peter Downey <peterdowney@visistruct.com>
2025-01-30 15:09:46 -0500

    Fix Broken Sites
    The major update this morning caused some sites to have issues due to 
    the wp-json location changes. Rolling them back until the errors can be 
    tested.
    
    The emergency auto update cron will update the changes at minute 1 of 
    the next hour. If you've disabled this function, run:
    
    cp -a /usr/local/bin/enginescript/etc/nginx/globals/wpsecure.conf 
    /etc/nginx/globals/wpsecure.conf
    
    service nginx restart

--- scripts/functions/auto-upgrade/normal-auto-upgrade.sh
+++ scripts/functions/auto-upgrade/normal-auto-upgrade.sh
@@ -26,33 +26,8 @@ fi
 source /usr/local/bin/enginescript/enginescript-variables.txt
 source /home/EngineScript/enginescript-install-options.txt
 
-# Update default.conf
-cp -a /usr/local/bin/enginescript/etc/nginx/globals/default.conf /etc/nginx/globals/default.conf
-
 # Update wpsecure.conf
 cp -a /usr/local/bin/enginescript/etc/nginx/globals/wpsecure.conf /etc/nginx/globals/wpsecure.conf
-
-# Remove fastcgi_params
-sed -i "s|include fastcgi_params;||g" /etc/nginx/globals/*.conf
-sed -i "s|include fastcgi_params;||g" /etc/nginx/sites-available/yourdomain.com.conf
-sed -i "s|include fastcgi_params;||g" /etc/nginx/sites-enabled/*.conf
-
-# Change index.php$is_args$args;
-sed -i "s|index.php$is_args$args;|/index.php?$args;|g" /etc/nginx/globals/fcgicachelocation.conf
-
-# Change server_names_hash_max_size
-sed -i "s|server_names_hash_max_size 2048;|server_names_hash_max_size 512;|g" /etc/nginx/nginx.conf
-
-# Change server_names_hash_max_size
-sed -i "s|fastcgi_cache_valid 200 1h|fastcgi_cache_valid 200 2h|g" /etc/nginx/nginx.conf
-
-# Change server_names_hash_max_size
-sed -i "s|\"; expires -1 always;|\" always; expires -1;|g" /etc/nginx/globals/staticfiles.conf
-
-# Remove http to https redirect
-# This was moved to default.conf and converted into a universal redirect for all domains
-sed -i '/# Forward HTTP Traffic to HTTPS/,/^$/d' /etc/nginx/sites-available/yourdomain.com.conf
-sed -i '/# Forward HTTP Traffic to HTTPS/,/^$/d' /etc/nginx/sites-enabled/*.conf
 
 # Restart Services
 /usr/local/bin/enginescript/scripts/functions/alias/alias-restart.sh
