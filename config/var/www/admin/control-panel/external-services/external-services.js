6c2eb094512f2d98bfb1bd50de5ce54cc3fe6c3f
Peter Downey <peterdowney@visistruct.com>
2026-04-20 15:55:57 -0400

    Fix FontAwesome comment in external-services.js
    Corrected a comment regarding FontAwesome version and release date.

--- config/var/www/admin/control-panel/external-services/external-services.js
+++ config/var/www/admin/control-panel/external-services/external-services.js
@@ -21,7 +21,8 @@ const CATEGORY_ORDER = [
   'Security'
 ];
 
-// FontAwesome 7+ icon patterns
+// FontAwesome 7 icon patterns
+// FontAwesome 7 released Released July 22, 2025
 const FA_STYLE_PREFIX_PATTERN = /^fa-(?:solid|regular|brands|light|thin|duotone|sharp-(?:solid|regular|light|thin|duotone))$/;
 const FA_ICON_MODIFIER_PATTERN = /^fa-(?:2?xs|sm|lg|xl|2?xl|[1-9]x|10x|spin|pulse|beat|fade|beat-fade|bounce|shake|fw)$/;
 
